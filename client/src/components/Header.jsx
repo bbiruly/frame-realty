9797db11e62ce5e04c9ba81a019a41ee256e3730
Bhagwan Biruly <b.biruly@Bhagwans-MacBook-Air.local>
2024-12-22 09:59:44 +0530

    added url

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ export default function Header() {
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Frame</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-slate-700'>Realty</span>
           </h1>
         </Link>
         <form
